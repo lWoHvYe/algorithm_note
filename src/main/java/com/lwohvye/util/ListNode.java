8787f09db51c9f5c43e9a34018b36432054bcc39
王红岩 <lWoHvYe@lWoHvYedeMac-mini.local>
2021-10-28 07:45:43 +0800

    ListNode添加一个构造函数

--- src/main/java/com/lwohvye/util/ListNode.java
+++ src/main/java/com/lwohvye/util/ListNode.java
@@ -7,4 +7,9 @@ public class ListNode {
     ListNode(int val) {
         this.val = val;
     }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
