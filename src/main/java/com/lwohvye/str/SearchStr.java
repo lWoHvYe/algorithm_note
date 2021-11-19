fd60f333b456ccc62ad3625cb2ab09afd9fefa61
王红岩 lWoHvYe <lwohvye@outlook.com>
2021-11-19 17:55:33 +0800

    KMP算法

--- src/main/java/com/lwohvye/str/SearchStr.java
+++ src/main/java/com/lwohvye/str/SearchStr.java
@@ -5,58 +5,58 @@ public class SearchStr {
     // region KMP算法
 
     // KMP 算法
-    // ss: 原串(string)  pp: 匹配串(pattern)
-    public int strStrKmp(String ss, String pp) {
-        if (pp.isEmpty()) return 0;
-
-        // 构建next数组
-        int[] next = genNext(pp);
-
-        // kmp匹配
-        return kmp(ss, pp, next);
+    // ts: 原串(string)  ps: 匹配串(pattern)
+    public static int kmp(String ts, String ps) {
+        char[] t = ts.toCharArray();
+        char[] p = ps.toCharArray();
+        int i = 0; // 主串的位置
+        int j = 0; // 模式串的位置
+        // 生成next数组
+        int[] next = getNext(ps);
+        while (i < t.length && j < p.length) {
+            if (j == -1 || t[i] == p[j]) { // 当j为-1时，要移动的是i，当然j也要归0
+                // 初始及匹配成功时
+                i++;
+                j++;
+            } else {
+                // 匹配不成功 j = next(j)
+                j = next[j]; // j回到指定位置
+            }
+        }
+        if (j == p.length) {
+            // 整一段匹配成功，直接返回下标
+            return i - j;
+        } else {
+            // 未找到。返回-1
+            return -1;
+        }
     }
 
-    private int[] genNext(String pp) {
-        var m = pp.length();
+    public static int[] getNext(String ps) {
+        char[] p = ps.toCharArray();
         // 构建 next 数组，数组长度为匹配串的长度（next 数组是和匹配串相关的）
-        int[] next = new int[m];
-        // 构造过程 i = 1，j = 0 开始，i 小于等于匹配串长度 【构造 i 从 1 开始】
-        for (int i = 1, j = 0; i < m; i++) {
-            while (j > 0 && pp.charAt(i) != pp.charAt(j)) {
-                // 匹配不成功的话，j = next(j - 1)
-                j = next[j - 1];
+        int[] next = new int[p.length];
+        // next[0] = -1 本身没太多的意义
+        next[0] = -1;
+        int j = 0;
+        int k = -1;
+        while (j < p.length - 1) {
+            if (k == -1 || p[j] == p[k]) {
+                // 初始及匹配成功时
+                if (p[++j] == p[++k]) { // 当两个字符相等时要跳过
+                    next[j] = next[k];
+                } else {
+                    // 更新 next[j]，结束本次循环，开始下一轮
+                    next[j] = k;
+                }
+            } else {
+                // 匹配不成功的话，k = next(k)
+                k = next[k];
             }
-            if (pp.charAt(i) == pp.charAt(j)) {
-                // 匹配成功的话，先让 j++
-                ++j;
-            }
-            // 更新 next[i]，结束本次循环，i++
-            next[i] = j;
         }
         return next;
     }
 
-    private int kmp(String ss, String pp, int[] next) {
-        // 分别读取原串和匹配串的长度
-        int n = ss.length(), m = pp.length();
-        // 匹配过程，i = 0，j = 0 开始，i 小于等于原串长度 【匹配 i 从 0 开始】
-        for (int i = 0, j = 0; i < n; ++i) {
-            while (j > 0 && ss.charAt(i) != pp.charAt(j)) {
-                // 匹配不成功 j = next(j-1)
-                j = next[j - 1];
-            }
-            if (ss.charAt(i) == pp.charAt(j)) {
-                // 匹配成功的话，先让 j++，结束本次循环后 i++
-                ++j;
-            }
-            if (j == m) {
-                // 整一段匹配成功，直接返回下标
-                return i - m + 1;
-            }
-        }
-        // 未找到。返回-1
-        return -1;
-    }
     // endregion
 
 
