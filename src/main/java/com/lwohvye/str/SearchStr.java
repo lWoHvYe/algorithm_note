28727b07392bfd8c8a427f334159bfe6deeb8f3a
王红岩 lWoHvYe <lwohvye@outlook.com>
2021-11-19 17:20:25 +0800

    KMP比较字符串

--- src/main/java/com/lwohvye/str/SearchStr.java
+++ src/main/java/com/lwohvye/str/SearchStr.java
@@ -1,14 +1,23 @@
 package com.lwohvye.str;
 
 public class SearchStr {
+
+    // region KMP算法
+
     // KMP 算法
     // ss: 原串(string)  pp: 匹配串(pattern)
     public int strStrKmp(String ss, String pp) {
         if (pp.isEmpty()) return 0;
 
-        // 分别读取原串和匹配串的长度
-        int n = ss.length(), m = pp.length();
+        // 构建next数组
+        int[] next = genNext(pp);
 
+        // kmp匹配
+        return kmp(ss, pp, next);
+    }
+
+    private int[] genNext(String pp) {
+        var m = pp.length();
         // 构建 next 数组，数组长度为匹配串的长度（next 数组是和匹配串相关的）
         int[] next = new int[m];
         // 构造过程 i = 1，j = 0 开始，i 小于等于匹配串长度 【构造 i 从 1 开始】
@@ -24,7 +33,12 @@ public class SearchStr {
             // 更新 next[i]，结束本次循环，i++
             next[i] = j;
         }
+        return next;
+    }
 
+    private int kmp(String ss, String pp, int[] next) {
+        // 分别读取原串和匹配串的长度
+        int n = ss.length(), m = pp.length();
         // 匹配过程，i = 0，j = 0 开始，i 小于等于原串长度 【匹配 i 从 0 开始】
         for (int i = 0, j = 0; i < n; ++i) {
             while (j > 0 && ss.charAt(i) != pp.charAt(j)) {
@@ -43,4 +57,7 @@ public class SearchStr {
         // 未找到。返回-1
         return -1;
     }
+    // endregion
+
+
 }
